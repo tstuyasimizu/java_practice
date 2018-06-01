adb8d296d2106787d69f59fe01ef30e33aeb2d0f
tatuyasimizu <tatsuya_shimizu@unirita.co.jp>
2018-06-01 14:45:13 +0900

    procon renshyu1_A commit

---
+++ Renshu_1_A.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Renshu_1_A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str.replace("ta",""));
+    }
+}
